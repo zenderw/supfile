ce1fba67dc3a4fc87c354926a7f2af30caa2bb06
zenderw <zenderwayl@gmail.com>
2026-02-16 13:07:33 +0200

    feat(shared): point d'entrée index.ts qui re-exporte tout

---
+++ packages/shared/src/index.ts
@@ -0,0 +1,7 @@
+export * from './types/user';
+export * from './types/folder';
+export * from './types/file';
+
+export * from './constants/limits';
+export * from './constants/mime';
+export * from './constants/errors';
