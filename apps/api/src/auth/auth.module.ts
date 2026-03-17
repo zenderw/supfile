369cc3ed92ad3259517a82a3edef8424b868636a
zenderw <zenderwayl@gmail.com>
2026-03-17 22:21:48 +0200

    feat(auth/api): enregistrement du JwtModule et des nouveaux services

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 
+import { AuthController } from './auth.controller';
+import { AuthService } from './services/auth.service';
 import { HashService } from './services/hash.service';
+import { TokenService } from './services/token.service';
 
 @Module({
-  providers: [HashService],
-  exports: [HashService],
+  imports: [JwtModule.register({})],
+  controllers: [AuthController],
+  providers: [HashService, TokenService, AuthService],
+  exports: [HashService, TokenService],
 })
 export class AuthModule {}
